ea97341790b73fe03e23652bdb147bb032eddc01
Elia <eliamatt2000@gmail.com>
2025-11-03 12:50:18 +0100

    Deleted random things

--- facpl-ngac-compare/src/main/java/policyCompare/NgacEvaluator.java
+++ facpl-ngac-compare/src/main/java/policyCompare/NgacEvaluator.java
@@ -1,21 +1,7 @@
 package policyCompare;
 
-import gov.nist.ngac.model.*;
-import gov.nist.ngac.pdp.*;
+import gov.nist.csd.pm.core.epp.*;
 
 public class NgacEvaluator {
 
-    public static boolean evaluate(String role, String action, String type) {
-        // Mapping: role -> UA, action -> OP, type -> OA
-        boolean doctor = role.equals("doctor");
-        boolean pharmacist = role.equals("pharmacist");
-        boolean ePrescription = type.equals("e-Prescription");
-
-        if (ePrescription) {
-            if (doctor && (action.equals("read") || action.equals("write"))) return true;
-            if (pharmacist && action.equals("read")) return true;
-        }
-
-        return false;
-    }
 }
